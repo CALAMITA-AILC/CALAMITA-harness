798a42313392a8c16b99d7fddc82fccfd9590578
Jj-source <gilijacopo@gmail.com>
2024-11-13 13:59:25 +0100

    blm now working, f1 score alwasy zero but could be ok

--- lm_eval/tasks/blm-IT/utils.py
+++ lm_eval/tasks/blm-IT/utils.py
@@ -1,8 +1,4 @@
 from sklearn.metrics import f1_score
-import datasets
-import re
-import numpy as np
-from sklearn.preprocessing import MultiLabelBinarizer
 
 def doc_to_text(doc) -> str:
   Context_concatenated= doc['Context_concatenated']
@@ -23,36 +19,9 @@ def preprocess_dataset(dataset):
     dataset = dataset.select([i for i in range(1)])      # selecting 4 rows for DEBUG
     return dataset
 
-
-# Funzioni passthrough per le metriche f1 da fare con aggregazione
-def f1_score(input):
-  f1_score_func()
-##
-
-def process_results(doc, results):
-  # doc: original doc
-  # results[0]: string output of the model
-  print(doc)
-  print(results[0])
-   
-
-def f1_score_func(y_true, y_pred):
-    return f1_score(y_true, y_pred, labels=["A", "B", "C", "D", "E", "F", "G", "H"] ,average='macro')
-
-# Funzioni di aggregazione che vengono richiamate direttamente dallo YAML [PER IL PROBLEMA BINARIO]
-def f1_score_agg2(dati):
-    correct_answers = np.array(["".join(sublist[0]) for sublist in dati])
-    generations = np.array(["".join(sublist[1][0]) for sublist in dati])
-    print("!!!!")
-    print("!!!")
-    print(dati)
-    print("!!!")
-    print("!!!")
-    return f1_score_func(correct_answers,generations)
-
-def f1_score_agg(items):
-  unzipped_list = list(zip(*items))
-  golds = unzipped_list[0]
-  preds = unzipped_list[1]
-  fscore = f1_score(golds, preds, average="macro")
-  return fscore
+def macro_f1_score(items):
+    unzipped_list = list(zip(*items))
+    golds = unzipped_list[0]
+    preds = unzipped_list[1]
+    fscore = f1_score(golds, preds, average="macro")
+    return fscore
